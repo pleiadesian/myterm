328ee26c22619564d6d4d52e1c982c8500c8634c
wzl <wangzl31@outlook.com>
2021-06-20 20:29:52 +0800

    Update myvim.sh

--- myvim.sh
+++ myvim.sh
@@ -28,7 +28,68 @@ set -g @plugin 'fcsonline/tmux-thumbs'
 # set -g @plugin 'git@bitbucket.com:user/plugin'
 
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-run '~/.tmux/plugins/tpm/tpm'" >> ~/.tmux.conf
+run '~/.tmux/plugins/tpm/tpm'
+#### COLOUR (base16-solarized)
+
+# This theme is a 256 color variant and it uses the color codes of the
+# base16-solarized (256 color) terminal/shell theme:
+# https://github.com/chriskempson/base16
+# https://github.com/chriskempson/base16-shell
+
+# It is based on the original tmux-colors-solarized light theme with some color
+# codes changed (to map solarized base2, base00, orange, base1).
+
+# In fact, this theme may be used in combination with any base16 256 color
+# terminal/shell theme. But it will probably look a bit 'burnt' (i.e.
+# solarized).  It better matches the base16-solarized-light and
+# base16-solarized-dark terminal/shell color themes.
+
+# default statusbar colors
+set-option -g status-style fg=colour17,bg=colour28 #yellow and base2
+
+# default window title colors
+set-window-option -g window-status-style fg=colour17,bg=default #base00 and default
+#set-window-option -g window-status-style dim
+
+# active window title colors
+set-window-option -g window-status-current-style fg=colour17,bg=default #orange and default
+#set-window-option -g window-status-current-style bright
+
+# pane border
+set-option -g pane-border-style fg=colour28 #base2
+set-option -g pane-active-border-style fg=colour22 #base1
+
+# message text
+set-option -g message-style fg=colour16,bg=colour18 #orange and base2
+
+# pane number display
+set-option -g display-panes-active-colour blue #blue
+set-option -g display-panes-colour colour16 #orange
+
+# clock
+set-window-option -g clock-mode-colour green #green
+
+# bell
+set-window-option -g window-status-bell-style fg=colour18,bg=red #base2, red)
+# Linux only
+set -g mouse on
+bind -n WheelUpPane if-shell -F -t = \"#{mouse_any_flag}\" \"send-keys -M\" \"if -Ft= '#{pane_in_mode}' 'send-keys -M' 'select-pane -t=; copy-mode -e; send-keys -M'\"
+bind -n WheelDownPane select-pane -t= \; send-keys -M
+bind -n C-WheelUpPane select-pane -t= \; copy-mode -e \; send-keys -M
+bind -T copy-mode-vi    C-WheelUpPane   send-keys -X halfpage-up
+bind -T copy-mode-vi    C-WheelDownPane send-keys -X halfpage-down
+bind -T copy-mode-emacs C-WheelUpPane   send-keys -X halfpage-up
+bind -T copy-mode-emacs C-WheelDownPane send-keys -X halfpage-down
+
+# To copy, left click and drag to highlight text in yellow,
+# once you release left click yellow text will disappear and will automatically be available in clibboard
+# # Use vim keybindings in copy mode
+setw -g mode-keys vi
+# Update default binding of `Enter` to also use copy-pipe
+unbind -T copy-mode-vi Enter
+bind-key -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel \"xclip -selection c\"
+bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel \"xclip -in -selection clipboard\"
+" >> ~/.tmux.conf
 cat tmuxcolors.conf >> ~/.tmux.conf 
 tmux source-file ~/.tmux.conf
 
